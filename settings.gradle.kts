50c30e6ee988fbd5164c1d03c3b97843a93e67dc
xx-den-xx <denbelobaba@gmail.com>
2023-05-01 23:40:02 +0300

    update gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -13,4 +14,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "SalesAndMoney"
-include ':app'
+include(":app")
