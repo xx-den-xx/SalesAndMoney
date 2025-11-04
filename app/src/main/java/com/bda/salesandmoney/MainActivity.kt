c00323c119de2f04620c87fdba3c9978a1408e48
mvoro <mvorontsov713@gmail.com>
2025-11-04 18:17:58 +0100

    Commit - connection check3

--- app/src/main/java/com/bda/salesandmoney/MainActivity.kt
+++ app/src/main/java/com/bda/salesandmoney/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.bda.salesandmoney.ui.theme.SalesAndMoneyTheme
 
+// connection check3
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
