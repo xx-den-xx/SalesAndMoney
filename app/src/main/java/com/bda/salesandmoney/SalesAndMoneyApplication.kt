b83e5aa09a5647f0907cef4b006ce045e6306d62
denisbelobaba <denbelobaba@gmail.com>
2025-11-04 11:16:23 +0300

    update gradle

---
+++ app/src/main/java/com/bda/salesandmoney/SalesAndMoneyApplication.kt
@@ -0,0 +1,10 @@
+package com.bda.salesandmoney
+
+import android.app.Application
+
+class SalesAndMoneyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
