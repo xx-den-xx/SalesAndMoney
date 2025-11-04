029eb411d5d04aab79e7e5c91876cd4799f4213d
Denis <denbelobaba@gmail.com>
2025-11-04 20:18:36 +0300

    Update MainActivity.kt

--- app/src/main/java/com/bda/salesandmoney/MainActivity.kt
+++ app/src/main/java/com/bda/salesandmoney/MainActivity.kt
@@ -12,8 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.bda.salesandmoney.ui.theme.SalesAndMoneyTheme
 
-// Сonnection check
-// Сonnection check 2
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
