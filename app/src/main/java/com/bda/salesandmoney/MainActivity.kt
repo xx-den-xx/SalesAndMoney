c59ae7a6270710e7c2a8396951c55cc6d1bdb000
Michael <47576594+michw85@users.noreply.github.com>
2025-11-04 13:20:43 +0100

    Update MainActivity.kt
    проверка соединения

--- app/src/main/java/com/bda/salesandmoney/MainActivity.kt
+++ app/src/main/java/com/bda/salesandmoney/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.bda.salesandmoney.ui.theme.SalesAndMoneyTheme
-
+// Привет из Гита)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
