db26cffadeb0d02136d825b93d189c675fceb8e6
denisbelobaba <denbelobaba@gmail.com>
2025-11-04 14:56:31 +0300

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,41 @@
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version("8.0.0") apply false
-    id("com.android.library") version("8.0.0") apply false
-    id("org.jetbrains.kotlin.android") version("1.7.20") apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.jetbrainsKotlinJvm) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
+    alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.androidx.room) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.baselineprofile) apply false
+    alias(libs.plugins.compose.compiler) apply false
+}
+
+// TODO Replace Detekt Gradle with terminal launch
+allprojects.onEach { project ->
+    project.afterEvaluate {
+        with(project.plugins) {
+            if (hasPlugin(libs.plugins.jetbrainsKotlinAndroid.get().pluginId) ||
+                hasPlugin(libs.plugins.jetbrainsKotlinJvm.get().pluginId)
+            ) {
+                apply(libs.plugins.detekt.get().pluginId)
+
+                project.extensions.configure<DetektExtension> {
+                    config.setFrom(rootProject.files("default-detekt-config.yml"))
+                }
+
+                project.dependencies.add("detektPlugins", libs.detekt.formatting.get().toString())
+            }
+
+            if (hasPlugin(libs.plugins.compose.compiler.get().pluginId)) {
+                project.dependencies.add("detektPlugins", libs.detekt.rules.compose.get().toString())
+            }
+        }
+    }
 }
